fe46fe6e46dd0a0f166726eac7b50e21ce3e0f66
zhafraan <aqiiizhaf@gmail.com>
2026-02-27 14:01:51 +0700

    pertemuan tambahan modul titik

---
+++ Pertemuan2/src/MTitik.java
@@ -0,0 +1,53 @@
+public class MTitik {
+    public static void main(String[] args) {
+        // Membuat objek Titik T1 dan mengatur koordinatnya
+        Titik T1 = new Titik(); 
+        T1.setAbsis(5);         
+        T1.setOrdinat(6);       
+        T1.printTitik();   
+        
+        System.err.println("Titik berada pada kuadran " + T1.getkuadran());
+        System.err.println("Jarak titik T1 dari titik pusat adalah " + T1.getjarakPusat());
+        // Membuat objek Titik T2 dan mengatur koordinatnya
+        Titik T2 = new Titik();
+        T2.setAbsis(-3);
+        T2.setOrdinat(4);
+        T2.printTitik();
+        System.out.println("Titik berada pada kuadran: " + T2.getkuadran());
+        System.out.println("Jarak T2 dari titik pusat: " + T2.getjarakPusat());
+        // Menghitung jarak antara T1 dan T2
+        double jarak = T1.getjarak(T2);
+        System.out.println("Jarak antara T1 dan T2: " + jarak);
+        // Menggeser T1 
+        T1.geser(2, -3);
+        T1.printTitik();
+        System.out.println("Kuadran setelah digeser: " + T1.getkuadran());
+        // Mencari refleksi T2 terhadap sumbu X dan Y
+        Titik refleksiX = T2.getRefleksiX();
+        System.out.print("T2 asli     : ");
+        T2.printTitik();
+        System.out.print("Refleksi X  : ");
+        refleksiX.printTitik();
+        // Mencari refleksi T2 terhadap sumbu Y
+        Titik refleksiY = T2.getRefleksiY();
+        System.out.print("T2 asli     : ");
+        T2.printTitik();
+        System.out.print("Refleksi Y  : ");
+        refleksiY.printTitik();
+        // Refleksi T2 terhadap sumbu X dan Y secara langsung
+        System.out.print("Sebelum refleksi: ");
+        T2.printTitik();
+        T2.refleksiX();
+        System.out.print("Setelah refleksiX: ");
+        T2.printTitik();
+        T2.refleksiY();
+        System.out.print("Setelah refleksiY: ");
+        T2.printTitik();
+        // Membuat objek Titik T3 dengan koordinat default (0,0)
+        Titik T3 = new Titik();
+        T3.printTitik();
+        System.out.println("Kuadran: " + T3.getkuadran() + " (0 = titik pusat/sumbu)");
+        System.out.println("Jarak dari pusat: " + T3.getjarakPusat());
+
+    }
+}
---
+++ Pertemuan2/src/Titik.java
@@ -0,0 +1,100 @@
+/* Nama File    : Titik.java
+ * Deskripsi    : Berisi atribut dan method dalam class Titik
+ * Pembuat      : Aqiatillah Rezi Zhafran/24060124140124
+ * Tanggal      : 27 Februari 2025
+ */
+public class Titik {
+    /****** ATRIBUT ******/
+    double absis;
+    double ordinat;
+
+    /****** METHOD ******/
+    // Konstruktor untuk membuat titik (0,0)
+    Titik() {
+        absis = 0;
+        ordinat = 0;
+    }
+
+    // Mengembalikan nilai absis
+    double getAbsis() {
+        return absis;
+    }
+
+    // Mengembalikan nilai ordinat
+    double getOrdinat() {
+        return ordinat;
+    }
+
+    // Mengeset absis titik dengan nilai baru x
+    void setAbsis(double x) {
+        absis = x;
+    }
+
+    // Mengeset ordinat titik dengan nilai baru y
+    void setOrdinat(double y) {
+        ordinat = y;
+    }
+
+    // Menggeser nilai absis dan ordinat titik masing-masing sejauh x dan y
+    void geser(double x, double y) {
+        absis = absis + x;
+        ordinat = ordinat + y;
+    }
+
+    // Mencetak koordinat titik
+    void printTitik() {
+        System.out.println("Titik (" + absis + "," + ordinat + ")");
+    }
+
+    //Mencari Kuadran dari titik
+    int getkuadran() {
+        if (absis > 0 && ordinat > 0) {
+            return 1; // Kuadran I
+        } else if (absis < 0 && ordinat > 0) {
+            return 2; // Kuadran II
+        } else if (absis < 0 && ordinat < 0) {
+            return 3; // Kuadran III
+        } else if (absis > 0 && ordinat < 0) {
+            return 4; // Kuadran IV
+        } else {
+            return 0; 
+        }
+    }
+
+    //Mencari jarak dari titik pusat
+    double getjarakPusat() {
+        return Math.sqrt(absis * absis + ordinat * ordinat);
+    }
+
+    //Mencari jarak antara titik ini 
+    double getjarak(Titik T) {
+        double dx = this.absis - T.getAbsis();
+        double dy = this.ordinat - T.getOrdinat();
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+    //Refleksi titik terhadap sumbu X
+    void refleksiX() {
+        ordinat = -ordinat;
+    }
+    //Refleksi titik terhadap sumbu Y
+    void refleksiY() {
+        absis = -absis;
+    }
+
+    // getRefleksiX()
+    Titik getRefleksiX() {
+        Titik refleksi = new Titik();
+        refleksi.setAbsis(absis);
+        refleksi.setOrdinat(-ordinat);
+        return refleksi;
+        }
+
+    // getRefleksiY() 
+    Titik getRefleksiY() {
+        Titik refleksi = new Titik();
+        refleksi.setAbsis(-absis);
+        refleksi.setOrdinat(ordinat);
+        return refleksi;
+    }
+   
+} // end class Titik
