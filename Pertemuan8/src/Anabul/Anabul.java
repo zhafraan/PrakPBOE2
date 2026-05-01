d86d8b56feca31924e6b2f428b333890d5c6802b
zhafraan <aqiiizhaf@gmail.com>
2026-05-01 15:33:37 +0700

    baru yg anabul blm final

---
+++ Pertemuan8/src/Anabul/Anabul.java
@@ -0,0 +1,16 @@
+/* Nama File    : Anabul.java
+ * Pembuat      : Aqiatillah Rezi Zhafran
+ * NIM          : 24060124140124
+ * Tanggal      : 30 April 2026 */
+
+public class Anabul {
+    protected String nama;
+    public Anabul(String nama) { 
+        this.nama = nama; 
+    }
+
+    public String getNama() { 
+        return nama; 
+    }
+    
+}
