ac3ad7fa7949d5ad65e9f01f5b62dc1a6d433f52
zhafraan <aqiiizhaf@gmail.com>
2026-05-04 21:40:37 +0700

    benerin buat ulang biar ge konfilik tadi merah semua ga bisa jalan

---
+++ Pertemuan8/src/Anabul/Kucing.java
@@ -0,0 +1,18 @@
+/* Nama File    : Kucing.java
+ * Pembuat      : Aqiatillah Rezi Zhafran
+ * NIM          : 24060124140124
+ * Tanggal      : 30 April 2026 */
+
+package Anabul;
+public class Kucing extends Anabul {
+    protected double bobot;
+
+    public Kucing(String nama, double bobot) {
+        super(nama);
+        this.bobot = bobot;
+    }
+
+    public double getBobot() { 
+        return bobot; 
+    }
+}
